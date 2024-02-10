9ae253a292b6e8be0c209be3e7ef7b7070cd47a1
masumbillahnishan <masumbillahnishan@gmail.com>
2024-02-11 02:18:21 +0600

    added the header

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './componant/Header/Header'
+
+function App() {
+
+  return (
+    <div className='App'>
+      <Header></Header>
+      
+
+    </div>
+
+  )
+}
+
+export default App
